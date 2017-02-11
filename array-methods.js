0b1ff29db0741be9f5ae72a35d1d262bf0cb3606
ginagala3090 <ginagala3090@users.noreply.github.com>
2017-02-11 10:49:02 -1000

    worked on array-methods

--- array-methods.js
+++ array-methods.js
@@ -5,7 +5,9 @@ var dataset = require('./dataset.json');
   greater than 100000.00
   assign the resulting array to `hundredThousandairs`
 */
-var hundredThousandairs = null;
+var hundredThousandairs = dataset.bankBalances.filter((element, index, array) => {
+  return element.amount > 100000.00;
+});
 
 /*
   set a new key for each object in bankBalances named `rounded`
@@ -18,7 +20,13 @@ var hundredThousandairs = null;
     }
   assign the resulting array to `roundedDollar`
 */
-var roundedDollar = null;
+var roundedDollar = dataset.bankBalances.map((element, index, array) => {
+  return {
+    amount: element.amount,
+    state: element.state,
+    rounded: Math.round(element.amount)
+  };
+});
 
 /*
   set a the `amount` value for each object in bankBalances
@@ -30,10 +38,18 @@ var roundedDollar = null;
     }
   assign the resulting array to `roundedDime`
 */
-var roundedDime = null;
+var roundedDime = dataset.bankBalances.map((element, index, array) => {
+  return {
+    amount: Math.round(element.amount * 10) / 10,
+    state: element.state
+  };
+});
 
 // set sumOfBankBalances to the sum of all amounts in bankBalances
-var sumOfBankBalances = null;
+var sumOfBankBalances = dataset.bankBalances.reduce((previous, current, index, array) => {
+  var sum = previous + parseFloat(current.amount);
+  return Math.round(sum * 100) / 100;
+}, 0);
 
 /*
   set sumOfInterests to the sum of the 18.9% interest
@@ -47,7 +63,36 @@ var sumOfBankBalances = null;
     Delaware
   the result should be rounded to the nearest cent
  */
-var sumOfInterests = null;
+var sumOfInterests = dataset.bankBalances.filter((element, index, array) => {
+  var states = ['WI', 'IL', 'WY', 'OH', 'GA', 'DE'];
+  if(states.indexOf(element.state) > -1) {
+    return true;
+  } else {
+    return false;
+  }
+})
+.reduce((previous, current, index, array) => {
+  var sum = previous + (current.amount * (18.9 / 100));
+  return Math.round(sum * 100) / 100;
+}, 0);
+
+/*
+  aggregate the sum of bankBalance amounts
+  grouped by state
+  set stateSums to be a hash table
+    where the key is the two letter state abbreviation
+    and the value is the sum of all amounts from that state
+      the value must be rounded to the nearest cent
+ */
+var stateSums = dataset.bankBalances.reduce((accounts, currentAccount) => {
+  if(accounts.hasOwnProperty(currentAccount.state) === false) {
+    accounts[currentAccount.state] = 0;
+  }
+  accounts[currentAccount.state] += parseFloat(currentAccount.amount);
+  // round to cents
+  accounts[currentAccount.state] = Math.round(accounts[currentAccount.state] * 100) / 100; 
+  return accounts;
+}, {});
 
 /*
   set sumOfHighInterests to the sum of the 18.9% interest
@@ -63,17 +108,25 @@ var sumOfInterests = null;
     Delaware
   the result should be rounded to the nearest cent
  */
-var sumOfHighInterests = null;
-
-/*
-  aggregate the sum of bankBalance amounts
-  grouped by state
-  set stateSums to be a hash table
-    where the key is the two letter state abbreviation
-    and the value is the sum of all amounts from that state
-      the value must be rounded to the nearest cent
- */
-var stateSums = null;
+var sumOfHighInterests = Object.keys(stateSums)
+// filter out the states that we don't care about
+.filter((state) => ['WI', 'IL', 'WY', 'OH', 'GA', 'DE'].indexOf(state) === -1)
+// convert amount to be the interest
+.map((state) => {
+  return {
+    state,
+    interest: Math.round(stateSums[state] * 18.9) / 100
+  };
+})
+// use only interest amounts that are greater than 50,000
+.filter((account) => {
+  return account.interest > 50000;
+})
+// add all the states interests together
+.reduce((prevInterest, currentAccount) => {
+  return prevInterest + currentAccount.interest;
+}, 0);
+sumOfHighInterests = Math.round(sumOfHighInterests * 100) / 100;
 
 /*
   set lowerSumStates to an array containing
@@ -81,7 +134,11 @@ var stateSums = null;
   where the sum of amounts in the state is
     less than 1,000,000
  */
-var lowerSumStates = null;
+var lowerSumStates = Object.keys(stateSums).filter(state => {
+  if(stateSums[state] < 1000000) {
+    return state;
+  }
+});
 
 /*
   set higherStateSums to be the sum of 
